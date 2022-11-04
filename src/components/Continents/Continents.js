c9773fdd5e6d1f65e7dcca8b71ce407516756422
Negar Yousefian <negar@87-92-52-58.bb.dnainternet.fi>
2022-11-04 18:59:54 +0200

    add icon pack and add countries

--- src/components/Continents/Continents.js
+++ src/components/Continents/Continents.js
@@ -1,14 +1,47 @@
 import styles from './Continents.module.css';
 
-function Continents() {
+function Continents({ selectContinent }) {
+    const chooseContinent = (name) => () => {
+        selectContinent(name);
+    };
     return (
         <div className={styles.flexContainer}>
-            <button className={`${styles.flexItem} ${styles.europe}`} onClick={() => {}}>Europe</button>
-            <button className={`${styles.flexItem} ${styles.asia}`} onClick={() => {}}>Asia</button>
-            <button className={`${styles.flexItem} ${styles.africa}`} onClick={() => {}}>Africa</button>
-            <button className={`${styles.flexItem} ${styles.namerica}`} onClick={() => {}}>North America</button>
-            <button className={`${styles.flexItem} ${styles.samerica}`} onClick={() => {}}>South America</button>
-            <button className={`${styles.flexItem} ${styles.australia}`} onClick={() => {}}>Australia</button>
+            <button
+                className={`${styles.flexItem} ${styles.europe}`}
+                onClick={chooseContinent('europe')}
+                >
+                    Europe
+            </button>
+            <button
+                className={`${styles.flexItem} ${styles.asia}`}
+                onClick={chooseContinent('asia')}
+                >
+                Asia
+            </button>
+            <button
+                className={`${styles.flexItem} ${styles.africa}`}
+                onClick={chooseContinent('africa')}
+                >
+                Africa
+            </button>
+            <button
+                className={`${styles.flexItem} ${styles.namerica}`}
+                onClick={chooseContinent('north_america')}
+                >
+                    North America
+            </button>
+            <button
+                className={`${styles.flexItem} ${styles.samerica}`}
+                onClick={chooseContinent('south_america')}
+                >
+                    South America
+            </button>
+            <button
+                className={`${styles.flexItem} ${styles.australia}`}
+                onClick={chooseContinent('australia')}
+                >
+                    Australia
+            </button>
         </div>
     );
 }
--- src/pages/Destinations/Destinations.js
+++ src/pages/Destinations/Destinations.js
@@ -1,27 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Continents from '../../components/Continents/Continents';
+import Countries from '../../components/Countries/Countries';
 import MainHeader from '../../components/MainHeader/MainHeader';
 import styles from './Destinations.module.css';
 
 function Destinations() {
     const [destination, setDestination] = useState('');
+    const [continent, setContinent] = useState('');
+    const countriesRef = useRef();
     const handleChangeDestination = (e) => {
         setDestination(e.target.value);
     }
+    const selectContinent = (newContinent) => {
+        countriesRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        setContinent(newContinent);
+    }
+    useEffect(() => {
+        selectContinent(continent);
+    });
     return (
         <div className={styles.container}>
             <div className={styles.headContainer}>
                 <MainHeader title='Where do you want to be?' />
-                <input
-                    className={styles.searchbar}
-                    type='text'
-                    value={destination}
-                    onChange={handleChangeDestination}
-                    placeholder='Search destinations'
-                />
-                <input className={styles.submitBtn} type='submit' value='Go' disabled={!destination} />
+                <div className={styles.searchbarContainer}>
+                    <input
+                        className={styles.searchbar}
+                        type='text'
+                        value={destination}
+                        onChange={handleChangeDestination}
+                        placeholder='Search destinations'
+                    />
+                    <input className={styles.submitBtn} type='submit' value='Go' disabled={!destination} />
+                </div>
             </div>
-            <Continents />
+            <Continents selectContinent={selectContinent}/>
+            <div ref={countriesRef}>{continent && <Countries continent={continent}/>}</div>
         </div>
     );
 }
