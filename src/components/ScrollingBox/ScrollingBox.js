aadb4de88ac8a74313d2566ad927976a5f9d89ed
Negar Yousefian <negar@Negars-MacBook-Pro.local>
2022-11-22 13:55:16 +0200

    create home page

---
+++ src/components/ScrollingBox/ScrollingBox.js
@@ -0,0 +1,22 @@
+import MainHeader from '../MainHeader/MainHeader';
+import styles from './ScrollingBox.module.css';
+import pic from '../../images/liam-burnett-blue-xBnqIf2vy7M-unsplash.jpg';
+
+function ScrollingBox({ title, caption, elements }) {
+    return (
+        <div className={styles.scrollContainer}>
+            <MainHeader title={title} />
+            <p className={styles.caption}>{caption}</p>
+            <ul className={styles.elementsContainer}>
+                {elements.map((e, index) =>
+                    <li className={styles.element} key={index}>
+                        <img src={pic} alt={e.name} />
+                        <p className={styles.elementName}>{e.name}</p>
+                    </li>
+                )}
+            </ul>
+        </div>
+    );
+}
+
+export default ScrollingBox;
